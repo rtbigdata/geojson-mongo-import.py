57cf1e8003f5d89cc3e78a32bff42fc773a7b67c
Rick Ciesla <esckey71@hotmail.com>
2017-05-23 15:56:44 -0400

    Implement bulk import for performance

--- geojson-mongo-import.py
+++ geojson-mongo-import.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# import GeoJSON file into MongoDB
+# Bulk import GeoJSON file into MongoDB
+# Note: bulk operations require MongoDB 3.2 or higher
 #
 # example usage:
 # given an input file named "points.geojson" having GeoJSON points
@@ -14,6 +15,7 @@
 import sys, json
 from datetime import datetime
 from pymongo import MongoClient, GEOSPHERE
+from pymongo.errors import (PyMongoError, BulkWriteError)
 
 inputfile = sys.argv[1]
 to_collection = sys.argv[2]
@@ -25,13 +27,31 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['geospatial']
 collection = db[to_collection]
 
+# create 2dsphere index and initialize unordered bulk insert
 collection.create_index([("geometry", GEOSPHERE)])
+bulk = collection.initialize_unordered_bulk_op()
 
 for feature in geojson['features']:
-  # Note: comment out the next two lines if your input file does not have a timestamp field in properties
+  # Note: comment out next two lines if input file does not contain timestamp field having proper format
   # timestamp = feature['properties']['timestamp']
   # feature['properties']['timestamp'] = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
-  collection.insert_one(feature)
 
+  # append to bulk insert list
+  bulk.insert(feature)
+
+# execute bulk operation to the DB
+try:
+  result = bulk.execute()
+  print "Number of Features successully inserted:", result["nInserted"]
+except BulkWriteError as bwe:
+  nInserted = bwe.details["nInserted"]
+  errMsg = bwe.details["writeErrors"]
+  print "Errors encountered inserting features"
+  print "Number of Features successully inserted:", nInserted
+  print "The following errors were found:"
+  for item in errMsg:
+    print "Index of feature:", item["index"]
+    print "Error code:", item["code"]
+    print "Message (truncated due to data length):", item["errmsg"][0:120], "..."
 
 
