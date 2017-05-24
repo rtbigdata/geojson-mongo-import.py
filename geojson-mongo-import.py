a611e62cb5761b30e85f957d1696bb11cbac18cf
Rick Ciesla <esckey71@hotmail.com>
2017-05-23 22:39:28 -0400

    add parameters for filename and DB connection

--- geojson-mongo-import.py
+++ geojson-mongo-import.py
@@ -8,23 +8,43 @@
 # given an input file named "points.geojson" having GeoJSON points
 # and destination collection named "points"
 #
-# python geojson-mongo-import.py points.geojson points
+# python geojson-mongo-import.py -f points.geojson -c points
 #
 #
 
-import sys, json
+import argparse, urllib, json
 from datetime import datetime
 from pymongo import MongoClient, GEOSPHERE
 from pymongo.errors import (PyMongoError, BulkWriteError)
 
-inputfile = sys.argv[1]
-to_collection = sys.argv[2]
+parser = argparse.ArgumentParser(description='Bulk import GeoJSON file into MongoDB')
+parser.add_argument('-f', required=True, help='input file')
+parser.add_argument('-s', default='localhost', help='target server name (default is localhost)')
+parser.add_argument('-port', default='27017', help='server port (default is 27017)')
+parser.add_argument('-d', required=True, help='target database name')
+parser.add_argument('-c', required=True, help='target collection to insert to')
+parser.add_argument('-u', help='username (optional)')
+parser.add_argument('-p', help='password (optional)')
+args = parser.parse_args()
+
+inputfile = args.f
+to_collection = args.c
+to_database = args.d
+to_server = args.s
+to_port = args.port
+db_user = args.u
+
+if db_user is None:
+  uri = 'mongodb://' + to_server + ':' + to_port +'/'
+else:
+  db_password = urllib.quote_plus(args.p)
+  uri = 'mongodb://' + db_user + ':' + db_password + '@' + to_server + ':' + to_port +'/' + to_database
 
 with open(inputfile,'r') as f:
   geojson = json.loads(f.read())
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client['geospatial']
+client = MongoClient(uri)
+db = client[to_database]
 collection = db[to_collection]
 
 # create 2dsphere index and initialize unordered bulk insert
